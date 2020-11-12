22c210999ff42785e32890957e6e3b72f9155d19
Yagub Shukurov <shukurov23@outlook.com>
2020-11-13 01:00:33 +0400

    Initial commit

---
+++ src/main/java/com/yagubshukurov/coronavirustracker/CoronavirusTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.yagubshukurov.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerApplication.class, args);
+
+	}
+
+}
---
+++ src/main/java/com/yagubshukurov/coronavirustracker/services/CoronaVirusDataService.java
@@ -0,0 +1,8 @@
+package com.yagubshukurov.coronavirustracker.services;
+
+
+public class CoronaVirusDataService {
+
+
+
+}
