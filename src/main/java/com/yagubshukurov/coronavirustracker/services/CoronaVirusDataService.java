e1f2d29e18b24411327433889e0656aa1aa662ff
Yagub Shukurov <shukurov23@outlook.com>
2020-11-13 03:16:06 +0400

    Fetch data and Arrange as CSV

--- src/main/java/com/yagubshukurov/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/yagubshukurov/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,8 +1,37 @@
 package com.yagubshukurov.coronavirustracker.services;
 
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
 public class CoronaVirusDataService {
 
+    private static String CORONAVIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    @PostConstruct
+    public void fetchData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(CORONAVIRUS_DATA_URL))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
 
+        StringReader csvReader = new StringReader(response.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvReader);
+        for (CSVRecord record : records) {
+            String country = record.get("Country/Region");
+            System.out.println(country);
+        }
+    }
 }
